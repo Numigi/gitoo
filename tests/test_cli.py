fd6fe308153736ebf28d41840389b63019162752
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2018-03-21 13:39:19 -0400

    Prevent installing modules inside folder that does not exist (#4)
    * Prevent installing modules inside folder that does not exist

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -63,47 +63,34 @@ class TestInstallOne(unittest.TestCase):
         self.assertFalse(os.path.exists(os.path.join(self.destination, 'setup')))
 
 
-class TestInstallAll(unittest.TestCase):
+class TestInstallBase(unittest.TestCase):
 
     def setUp(self):
-        super(TestInstallAll, self).setUp()
+        super(TestInstallBase, self).setUp()
         self.func = cli._install_all
         _, self.filename = tempfile.mkstemp()
         yaml_data = [
             {
                 "url": "https://github.com/odoo/odoo",
                 "branch": "10.0",
-                "commit": "ae6037e17acdc66cf28504d9791eb3cd698efaa6",
+                "commit": "1ad8867deea179e3548f8f7619fd1bd11d1446ae",
                 "patches": [
                     {
-                        "url": "https://github.com/savoirfairelinux/odoo",
-                        "branch": "10.0_ddufresne_fix_purchase",
-                        "commit": "5f9dd4d2861eeba40cf81e658b99a83d9f88d6e7"
-                    }
+                        "url": "https://github.com/ddufresne/odoo",
+                        "branch": "10.0-ddufresne-fix_aged_partner_balance",
+                        "commit": "97929246ca0c7df24b4d46adc27d440712687b5a"
+                    },
                 ],
                 "exclude_modules": ["web_tour"],
                 "base": True
             },
-            {
-                "url": "https://github.com/OCA/l10n-canada",
-                "branch": "10.0",
-                "commit": "9c1b75a837c0f15e127ee656e871b78db1407505",
-                "patches": [
-                    {
-                        "url": "https://github.com/savoirfairelinux/l10n-canada",
-                        "branch": "10.0-toponyms_translation",
-                        "commit": "40176efdbd2acbd4ae70e548af4a077afed11d5d"
-                    }
-                ],
-                "exclude_modules": ["setup"]
-            }
         ]
         with open(self.filename, 'w') as f:
             yaml.dump(yaml_data, f)
         self.destination = tempfile.mkdtemp()
 
     def tearDown(self):
-        super(TestInstallAll, self).tearDown()
+        super(TestInstallBase, self).tearDown()
         if os.path.exists(self.destination):
             shutil.rmtree(self.destination)
 
@@ -114,3 +101,48 @@ class TestInstallAll(unittest.TestCase):
             conf_file=self.filename
         )
         self.assertTrue(os.listdir(self.destination))
+
+
+class TestInstallThirdParty(unittest.TestCase):
+
+    def setUp(self):
+        super(TestInstallThirdParty, self).setUp()
+        self.func = cli._install_all
+        _, self.filename = tempfile.mkstemp()
+        yaml_data = [
+            {
+                "url": "https://github.com/OCA/server-tools",
+                "branch": "11.0",
+                "commit": "32291d5b6aed0a9a3400975288fc6bef2cb5f985",
+                "patches": [
+                    {
+                        "url": "https://github.com/ddufresne/server-tools",
+                        "branch": "11.0-mig-super_calendar",
+                        "commit": "34e67cab64250de60fa4e1c24cae3fbc3962a250"
+                    },
+                ]
+            },
+            {
+                "url": "https://github.com/OCA/website",
+                "branch": "11.0",
+                "commit": "899a2219d35a259422ce916ba99947108bc3cc3c",
+            },
+        ]
+        with open(self.filename, 'w') as f:
+            yaml.dump(yaml_data, f)
+        self.destination = tempfile.mkdtemp()
+
+    def tearDown(self):
+        super(TestInstallThirdParty, self).tearDown()
+        if os.path.exists(self.destination):
+            shutil.rmtree(self.destination)
+
+    def test_install_all(self):
+        self.assertFalse(os.listdir(self.destination))
+        self.func(destination=self.destination, conf_file=self.filename)
+        self.assertTrue(os.listdir(self.destination))
+
+    def test_destination_folder_does_not_exist(self):
+        destination = os.path.join(self.destination, 'addons')
+        with self.assertRaises(RuntimeError):
+            self.func(destination=destination, conf_file=self.filename)
