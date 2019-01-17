d5a9d2c4631f9cae273ba5a1c15be054bc0c9113
David Dufresne <david.dufresne@numigi.net>
2019-01-17 12:09:51 -0500

    Enable patching a repo from a .patch file.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -114,3 +114,42 @@ class TestInstallThirdPartyWithIncludes(ThirdPartyTestMixin):
         self.func(destination=self.destination, conf_file=self.filename)
         modules = os.listdir(self.destination)
         self.assertEqual(set(modules), set(self._included_modules))
+
+
+class TestPatchUsingFile(ThirdPartyTestMixin):
+
+    _patch_name = 'server-tools-sentry-readme.patch'
+
+    _yaml_data = [
+        {
+            "url": "https://github.com/OCA/server-tools",
+            "branch": "11.0",
+            "commit": "32291d5b6aed0a9a3400975288fc6bef2cb5f985",
+            "patches": [
+                {
+                    "file": "gitoo-patches/{}".format(_patch_name),
+                },
+            ]
+        },
+    ]
+
+    def setUp(self):
+        super(TestPatchUsingFile, self).setUp()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.patch_file = os.path.join(dir_path, 'patches', self._patch_name)
+        self.patches_dir = '/tmp/gitoo-patches'
+        os.makedirs(self.patches_dir, exist_ok=True)
+        shutil.copy2(self.patch_file, self.patches_dir)
+
+    def tearDown(self):
+        super(TestPatchUsingFile, self).tearDown()
+        if os.path.exists(self.patch_file):
+            shutil.rmtree(self.patches_dir)
+
+    def test_install_all(self):
+        self.assertFalse(os.listdir(self.destination))
+        self.func(destination=self.destination, conf_file=self.filename)
+
+        readme_file = os.path.join(self.destination, 'sentry', 'README.rst')
+        readme_content = open(readme_file, 'r').read()
+        self.assertIn('This is a patch.', readme_content)
